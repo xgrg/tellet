e308b98e6aff405daff5d498132857c40cc32bc3
xgrg <goperto@gmail.com>
2023-11-17 19:24:10 +0100

    fix host and port

--- tellet/__main__.py
+++ tellet/__main__.py
@@ -2,5 +2,5 @@ import uvicorn
 
 if __name__ == "__main__":
     uvicorn.run(
-        "tellet.main:app", host="127.0.0.1", port=5000, log_level="info", reload=True
+        "tellet.main:app", host="0.0.0.0", port=8888, log_level="info", reload=True
     )
